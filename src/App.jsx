878019bd1bdf7c8b85dbacf47a1807e516dcf01d
Ehsan Ojaghloo <e.ojaghloo86@gmail.com>
2024-07-06 21:09:44 +0330

    added a descending and ascending function

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,53 @@
-import React , {useRef , useState , useEffect} from 'react'
+import React , {useState , useEffect} from 'react'
 import "./App.scss"
 
 function App() {
 
   const [products , setProducts] = useState([]);
+  const [sorted , setSorted] = useState(false);
 
   useEffect(()=>{
-    fetch('https://api.escuelajs.co/api/v1/products?offset=3&limit=8')
-      .then(res => res.json())
-      .then((output) => {
-        console.log(output)
-        setProducts(output)
+    fetch('https://fakestoreapi.com/products')
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json);
+        setProducts(json);
       })
-  }, [])
+  }, []);
+
+  const ascSorted = [...products].sort((a,b)=> a.price - b.price);
+  const descSorted = [...products].sort((a,b)=> b.price - a.price);
 
   return (
-    <div className='items-con'>
-      {products.map((data) =>(
-        <div key={data.id} className='items'>
-          <img src={data.images}/> 
-          <h1><b>{data.title}</b></h1>
-          <h2>{data.price}</h2>
-          <hr/>
-        </div>
-      ))}
+    <div className='app-con'>
+      <div className="nav-con">
+        <nav>
+          <button className='asc' onClick={()=> setSorted(false)}>Asc</button>
+          <button className='desc' onClick={()=> setSorted(true)}>Desc</button>
+        </nav>
+      </div>
+      <div className='all-products'>
+        {sorted ? descSorted.map((data)=>(
+          <div key={data.id} className='products'>
+            <img src={data.images}/>
+            <div className='products-info'>
+              <h2>{data.title}</h2>
+              <p>{data.description.slice(0 , 180)}...</p>
+              <span>{data.price}$</span>
+            </div>
+          </div>
+        ))
+        : ascSorted.map((data)=>(
+          <div key={data.id} className='products'>
+            <img src={data.image}/>
+            <div className='products-info'>
+              <h2>{data.title}</h2>
+              <p>{data.description.slice(0 , 125)}...</p>
+              <span>{data.price}$</span>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
